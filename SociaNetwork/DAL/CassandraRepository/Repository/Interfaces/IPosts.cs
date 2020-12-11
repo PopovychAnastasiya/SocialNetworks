0d54ba514ae6388c0739614a9dd200c8fd80307f
PopovychAnastasiya <50025596+PopovychAnastasiya@users.noreply.github.com>
2020-12-11 02:05:25 +0200

    Add Cassandra

---
+++ SociaNetwork/DAL/CassandraRepository/Repository/Interfaces/IPosts.cs
@@ -0,0 +1,15 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.CassandraRepository.Repository.Interfaces
+{
+    interface IPosts
+    {
+        void CreatePost(ISession session);
+        void UpdatePost(ISession session);
+    }
+}
